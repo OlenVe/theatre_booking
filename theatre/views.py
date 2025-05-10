a78c18c142c47b66f822274fd921697e9c661bce
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-05-10 14:59:40 +0200

    add permission IsAdminOrIfAuthenticated ReadOnly

--- theatre/views.py
+++ theatre/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from rest_framework import permissions
+
+from theatre.models import Genre
+
 
 # Create your views here.
 class GenreViewSet:
-    pass
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class ActorViewSet:
